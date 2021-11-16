c9dfd6dcfda47c675acf28c63aeecd0f72d031d9
LorenzoGirinelli <girinellilorenzo22@hotmail.com>
2021-11-16 10:18:54 +0100

    finito

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
-const element = document.querySelector(`.header-right>a`);
-element.addEventListener(`click`,
- function() {
-
- }
+// ATTIVAZIONE MENU
+const burgerButton = document.querySelector(`.header-right>a`);
+burgerButton.addEventListener(`click`,
+    function() {
+        const hamburgerMenu = document.querySelector(`.hamburger-menu`);
+        hamburgerMenu.classList.add(`active`);
+    }
+)
+// CHIUSURA MENU
+const closeButton = document.querySelector(`.close`);
+closeButton.addEventListener(`click`,
+    function() {
+        const hamburgerMenu = document.querySelector(`.hamburger-menu`);
+        hamburgerMenu.classList.remove(`active`);
+    }
 )
